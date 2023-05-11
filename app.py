480fbe3e2c2f849d178322824a6630963857c728
gracecai <caixinlei@sjtu.edu.cn>
2023-05-11 13:27:49 +0800

    no key

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, jsonify
 import openai
-openai.api_key = "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"
+openai.api_key = ""
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/email-generator')
 def email_generator():
     return render_template('email-generator.html')
+
 
 def email_writer(recipient, addressor, style, content):
     prompt = f'Please write an email for me. The recipient is {recipient}.The addressor is {addressor}. The style of email is {style}. The content is {content}.'
@@ -22,12 +25,12 @@ def email_writer(recipient, addressor, style, content):
             {"role": "user", "content": prompt}
         ],
         temperature=0,
-        max_tokens = 250
+        max_tokens=250
     )
     email = response['choices'][0]['message']['content']
     return email
 
-    
+
 @app.route('/generate-email', methods=['POST'])
 def generate_email():
     data = request.json
@@ -35,12 +38,13 @@ def generate_email():
     addressor = data['addressor']
     style = data['style']
     content = data['content']
-    
+
     # Generate email based on input data
     email = email_writer(recipient, addressor, style, content)
-    
+
     # Return generated email as JSON response
     return jsonify({'email': email})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
